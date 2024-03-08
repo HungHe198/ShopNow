0e9202a73ca6cad66cdc887f0fbc5d5fa98f5f60
HungHe198 <hunglvph45911@fpt.edu.vn>
2024-03-08 08:13:27 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using ShopNow.Source_Code.GUI.VIEWS.Login;
+
+namespace ShopNow
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Login());
+        }
+    }
+}
