e3c25efb2c4146c17f61b920dbd696ee2b3e6945
ADMIN <ADMIN@Eng>
2024-03-23 16:42:16 +0700

    ...

---
+++ Source Code/GUI/VIEWS/Admin/Quản lí nhân viên.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ShopNow.Source_Code.GUI.VIEWS.Admin
+{
+    partial class Quản_lí_nhân_viên
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Quản_lí_nhân_viên));
+            grb_Quanlinhanvien = new GroupBox();
+            dataGridView1 = new DataGridView();
+            btn_Quanlithongke = new Button();
+            btn_Quanlicoso = new Button();
+            btn_QLGG = new Button();
+            btn_QLNH = new Button();
+            btn_QLNV = new Button();
+            btn_QLSP = new Button();
+            ptb_Quanlisanpham = new PictureBox();
+            button1 = new Button();
+            grb_Quanlinhanvien.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ptb_Quanlisanpham).BeginInit();
+            SuspendLayout();
+            // 
+            // grb_Quanlinhanvien
+            // 
+            grb_Quanlinhanvien.Controls.Add(dataGridView1);
+            grb_Quanlinhanvien.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            grb_Quanlinhanvien.Location = new Point(223, 4);
+            grb_Quanlinhanvien.Name = "grb_Quanlinhanvien";
+            grb_Quanlinhanvien.Size = new Size(966, 747);
+            grb_Quanlinhanvien.TabIndex = 4;
+            grb_Quanlinhanvien.TabStop = false;
+            grb_Quanlinhanvien.Text = "Quản lí nhân viên";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(6, 184);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(954, 557);
+            dataGridView1.TabIndex = 0;
+            // 
+            // btn_Quanlithongke
+            // 
+            btn_Quanlithongke.Location = new Point(12, 462);
+            btn_Quanlithongke.Name = "btn_Quanlithongke";
+            btn_Quanlithongke.Size = new Size(205, 45);
+            btn_Quanlithongke.TabIndex = 6;
+            btn_Quanlithongke.Text = "Quản lí thống kê";
+            btn_Quanlithongke.UseVisualStyleBackColor = true;
+            // 
+            // btn_Quanlicoso
+            // 
+            btn_Quanlicoso.Location = new Point(12, 411);
+            btn_Quanlicoso.Name = "btn_Quanlicoso";
+            btn_Quanlicoso.Size = new Size(205, 45);
+            btn_Quanlicoso.TabIndex = 7;
+            btn_Quanlicoso.Text = "Quản lí cơ sở";
+            btn_Quanlicoso.UseVisualStyleBackColor = true;
+            // 
+            // btn_QLGG
+            // 
+            btn_QLGG.Location = new Point(12, 360);
+            btn_QLGG.Name = "btn_QLGG";
+            btn_QLGG.Size = new Size(205, 45);
+            btn_QLGG.TabIndex = 8;
+            btn_QLGG.Text = "Quản lí giảm giá";
+            btn_QLGG.UseVisualStyleBackColor = true;
+            // 
+            // btn_QLNH
+            // 
+            btn_QLNH.Location = new Point(12, 309);
+            btn_QLNH.Name = "btn_QLNH";
+            btn_QLNH.Size = new Size(205, 45);
+            btn_QLNH.TabIndex = 9;
+            btn_QLNH.Text = "Quản lí nhập hàng";
+            btn_QLNH.UseVisualStyleBackColor = true;
+            // 
+            // btn_QLNV
+            // 
+            btn_QLNV.Location = new Point(12, 258);
+            btn_QLNV.Name = "btn_QLNV";
+            btn_QLNV.Size = new Size(205, 45);
+            btn_QLNV.TabIndex = 10;
+            btn_QLNV.Text = "Quản lí nhân viên";
+            btn_QLNV.UseVisualStyleBackColor = true;
+            // 
+            // btn_QLSP
+            // 
+            btn_QLSP.Location = new Point(12, 207);
+            btn_QLSP.Name = "btn_QLSP";
+            btn_QLSP.Size = new Size(205, 45);
+            btn_QLSP.TabIndex = 11;
+            btn_QLSP.Text = "Quản lí sản phẩm";
+            btn_QLSP.UseVisualStyleBackColor = true;
+            // 
+            // ptb_Quanlisanpham
+            // 
+            ptb_Quanlisanpham.BackColor = SystemColors.ActiveCaption;
+            ptb_Quanlisanpham.Image = (Image)resources.GetObject("ptb_Quanlisanpham.Image");
+            ptb_Quanlisanpham.Location = new Point(12, 12);
+            ptb_Quanlisanpham.Name = "ptb_Quanlisanpham";
+            ptb_Quanlisanpham.Size = new Size(205, 170);
+            ptb_Quanlisanpham.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptb_Quanlisanpham.TabIndex = 5;
+            ptb_Quanlisanpham.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 513);
+            button1.Name = "button1";
+            button1.Size = new Size(205, 45);
+            button1.TabIndex = 6;
+            button1.Text = "...";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Quản_lí_nhân_viên
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1220, 778);
+            Controls.Add(button1);
+            Controls.Add(btn_Quanlithongke);
+            Controls.Add(btn_Quanlicoso);
+            Controls.Add(btn_QLGG);
+            Controls.Add(btn_QLNH);
+            Controls.Add(btn_QLNV);
+            Controls.Add(btn_QLSP);
+            Controls.Add(ptb_Quanlisanpham);
+            Controls.Add(grb_Quanlinhanvien);
+            Name = "Quản_lí_nhân_viên";
+            Text = "Quản_lí_nhân_viên";
+            grb_Quanlinhanvien.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ptb_Quanlisanpham).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox grb_Quanlinhanvien;
+        private DataGridView dataGridView1;
+        private Button btn_Quanlithongke;
+        private Button btn_Quanlicoso;
+        private Button btn_QLGG;
+        private Button btn_QLNH;
+        private Button btn_QLNV;
+        private Button btn_QLSP;
+        private PictureBox ptb_Quanlisanpham;
+        private Button button1;
+    }
+}
--- Source Code/GUI/VIEWS/Admin/Quản lí sản phẩm.Designer.cs
+++ Source Code/GUI/VIEWS/Admin/Quản lí sản phẩm.Designer.cs
@@ -28,180 +28,322 @@
         /// </summary>
         private void InitializeComponent()
         {
-            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            groupBox1 = new GroupBox();
-            button2 = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Quản_lí_sản_phẩm));
+            dataGridView1 = new DataGridView();
             button1 = new Button();
-            btn_QLKH = new Button();
-            btn_QLBH = new Button();
+            btn_Quanlithongke = new Button();
+            btn_Quanlicoso = new Button();
+            btn_QLGG = new Button();
+            btn_QLNH = new Button();
             btn_QLNV = new Button();
             btn_QLSP = new Button();
             ptb_Quanlisanpham = new PictureBox();
-            backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            grb_Quanlisanpham = new GroupBox();
+            btn_Themsanpham = new Button();
+            tb_Timkiem = new TextBox();
+            cmb_Sapxep = new ComboBox();
             groupBox2 = new GroupBox();
-            dataGridView1 = new DataGridView();
-            label1 = new Label();
-            backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
-            groupBox3 = new GroupBox();
-            groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)ptb_Quanlisanpham).BeginInit();
-            groupBox2.SuspendLayout();
+            cmb_Trangthai = new ComboBox();
+            tb_Gia = new TextBox();
+            tb_Tensanpham = new TextBox();
+            tb_Masanpham = new TextBox();
+            lb_Trangthai = new Label();
+            label2 = new Label();
+            lb_Tensanpham = new Label();
+            lb_Masanpham = new Label();
+            btn_Capnhat = new Button();
+            button3 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ptb_Quanlisanpham).BeginInit();
+            grb_Quanlisanpham.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
-            // groupBox1
+            // dataGridView1
             // 
-            groupBox1.Controls.Add(button2);
-            groupBox1.Controls.Add(button1);
-            groupBox1.Controls.Add(btn_QLKH);
-            groupBox1.Controls.Add(btn_QLBH);
-            groupBox1.Controls.Add(btn_QLNV);
-            groupBox1.Controls.Add(btn_QLSP);
-            groupBox1.Controls.Add(ptb_Quanlisanpham);
-            groupBox1.Location = new Point(12, 12);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(292, 754);
-            groupBox1.TabIndex = 0;
-            groupBox1.TabStop = false;
-            groupBox1.Text = "Chức năng";
-            // 
-            // button2
-            // 
-            button2.Location = new Point(6, 476);
-            button2.Name = "button2";
-            button2.Size = new Size(280, 45);
-            button2.TabIndex = 1;
-            button2.Text = "...";
-            button2.UseVisualStyleBackColor = true;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(6, 186);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(628, 566);
+            dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(6, 425);
+            button1.Location = new Point(12, 522);
             button1.Name = "button1";
-            button1.Size = new Size(280, 45);
-            button1.TabIndex = 1;
+            button1.Size = new Size(240, 45);
+            button1.TabIndex = 14;
             button1.Text = "...";
             button1.UseVisualStyleBackColor = true;
             // 
-            // btn_QLKH
+            // btn_Quanlithongke
             // 
-            btn_QLKH.Location = new Point(6, 374);
-            btn_QLKH.Name = "btn_QLKH";
-            btn_QLKH.Size = new Size(280, 45);
-            btn_QLKH.TabIndex = 1;
-            btn_QLKH.Text = "Quản lí khách hàng";
-            btn_QLKH.UseVisualStyleBackColor = true;
+            btn_Quanlithongke.Location = new Point(12, 471);
+            btn_Quanlithongke.Name = "btn_Quanlithongke";
+            btn_Quanlithongke.Size = new Size(240, 45);
+            btn_Quanlithongke.TabIndex = 15;
+            btn_Quanlithongke.Text = "Quản lí thống kê";
+            btn_Quanlithongke.UseVisualStyleBackColor = true;
             // 
-            // btn_QLBH
+            // btn_Quanlicoso
             // 
-            btn_QLBH.Location = new Point(6, 323);
-            btn_QLBH.Name = "btn_QLBH";
-            btn_QLBH.Size = new Size(280, 45);
-            btn_QLBH.TabIndex = 1;
-            btn_QLBH.Text = "Quản lí bán hàng";
-            btn_QLBH.UseVisualStyleBackColor = true;
+            btn_Quanlicoso.Location = new Point(12, 420);
+            btn_Quanlicoso.Name = "btn_Quanlicoso";
+            btn_Quanlicoso.Size = new Size(240, 45);
+            btn_Quanlicoso.TabIndex = 16;
+            btn_Quanlicoso.Text = "Quản lí cơ sở";
+            btn_Quanlicoso.UseVisualStyleBackColor = true;
+            // 
+            // btn_QLGG
+            // 
+            btn_QLGG.Location = new Point(12, 369);
+            btn_QLGG.Name = "btn_QLGG";
+            btn_QLGG.Size = new Size(240, 45);
+            btn_QLGG.TabIndex = 17;
+            btn_QLGG.Text = "Quản lí giảm giá";
+            btn_QLGG.UseVisualStyleBackColor = true;
+            // 
+            // btn_QLNH
+            // 
+            btn_QLNH.Location = new Point(12, 318);
+            btn_QLNH.Name = "btn_QLNH";
+            btn_QLNH.Size = new Size(240, 45);
+            btn_QLNH.TabIndex = 18;
+            btn_QLNH.Text = "Quản lí nhập hàng";
+            btn_QLNH.UseVisualStyleBackColor = true;
             // 
             // btn_QLNV
             // 
-            btn_QLNV.Location = new Point(6, 272);
+            btn_QLNV.Location = new Point(12, 267);
             btn_QLNV.Name = "btn_QLNV";
-            btn_QLNV.Size = new Size(280, 45);
-            btn_QLNV.TabIndex = 1;
+            btn_QLNV.Size = new Size(240, 45);
+            btn_QLNV.TabIndex = 19;
             btn_QLNV.Text = "Quản lí nhân viên";
             btn_QLNV.UseVisualStyleBackColor = true;
             // 
             // btn_QLSP
             // 
-            btn_QLSP.Location = new Point(6, 221);
+            btn_QLSP.Location = new Point(12, 216);
             btn_QLSP.Name = "btn_QLSP";
-            btn_QLSP.Size = new Size(280, 45);
-            btn_QLSP.TabIndex = 1;
+            btn_QLSP.Size = new Size(240, 45);
+            btn_QLSP.TabIndex = 20;
             btn_QLSP.Text = "Quản lí sản phẩm";
             btn_QLSP.UseVisualStyleBackColor = true;
             // 
             // ptb_Quanlisanpham
             // 
             ptb_Quanlisanpham.BackColor = SystemColors.ActiveCaption;
-            ptb_Quanlisanpham.Location = new Point(6, 26);
+            ptb_Quanlisanpham.Image = (Image)resources.GetObject("ptb_Quanlisanpham.Image");
+            ptb_Quanlisanpham.Location = new Point(12, 21);
             ptb_Quanlisanpham.Name = "ptb_Quanlisanpham";
-            ptb_Quanlisanpham.Size = new Size(280, 170);
-            ptb_Quanlisanpham.TabIndex = 0;
+            ptb_Quanlisanpham.Size = new Size(240, 189);
+            ptb_Quanlisanpham.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptb_Quanlisanpham.TabIndex = 13;
             ptb_Quanlisanpham.TabStop = false;
+            // 
+            // grb_Quanlisanpham
+            // 
+            grb_Quanlisanpham.Controls.Add(btn_Themsanpham);
+            grb_Quanlisanpham.Controls.Add(tb_Timkiem);
+            grb_Quanlisanpham.Controls.Add(cmb_Sapxep);
+            grb_Quanlisanpham.Controls.Add(dataGridView1);
+            grb_Quanlisanpham.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            grb_Quanlisanpham.Location = new Point(258, 21);
+            grb_Quanlisanpham.Name = "grb_Quanlisanpham";
+            grb_Quanlisanpham.Size = new Size(643, 760);
+            grb_Quanlisanpham.TabIndex = 12;
+            grb_Quanlisanpham.TabStop = false;
+            grb_Quanlisanpham.Text = "Quản lí sản phẩm";
+            // 
+            // btn_Themsanpham
+            // 
+            btn_Themsanpham.AutoSize = true;
+            btn_Themsanpham.BackColor = Color.Coral;
+            btn_Themsanpham.FlatAppearance.BorderColor = SystemColors.ActiveCaptionText;
+            btn_Themsanpham.FlatAppearance.MouseDownBackColor = Color.Black;
+            btn_Themsanpham.FlatAppearance.MouseOverBackColor = Color.Yellow;
+            btn_Themsanpham.FlatStyle = FlatStyle.Flat;
+            btn_Themsanpham.Font = new Font("Times New Roman", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_Themsanpham.ForeColor = Color.White;
+            btn_Themsanpham.Location = new Point(6, 50);
+            btn_Themsanpham.Name = "btn_Themsanpham";
+            btn_Themsanpham.Size = new Size(152, 38);
+            btn_Themsanpham.TabIndex = 6;
+            btn_Themsanpham.Text = "Thêm sản phẩm";
+            btn_Themsanpham.UseVisualStyleBackColor = false;
+            // 
+            // tb_Timkiem
+            // 
+            tb_Timkiem.BackColor = SystemColors.InactiveBorder;
+            tb_Timkiem.Location = new Point(6, 140);
+            tb_Timkiem.Name = "tb_Timkiem";
+            tb_Timkiem.PlaceholderText = "Nhập sản phẩm cần tìm";
+            tb_Timkiem.Size = new Size(356, 27);
+            tb_Timkiem.TabIndex = 5;
+            // 
+            // cmb_Sapxep
+            // 
+            cmb_Sapxep.FormattingEnabled = true;
+            cmb_Sapxep.Location = new Point(486, 139);
+            cmb_Sapxep.Name = "cmb_Sapxep";
+            cmb_Sapxep.Size = new Size(151, 28);
+            cmb_Sapxep.TabIndex = 3;
             // 
             // groupBox2
             // 
-            groupBox2.Controls.Add(dataGridView1);
-            groupBox2.Controls.Add(label1);
-            groupBox2.Location = new Point(310, 12);
+            groupBox2.Controls.Add(button3);
+            groupBox2.Controls.Add(btn_Capnhat);
+            groupBox2.Controls.Add(cmb_Trangthai);
+            groupBox2.Controls.Add(tb_Gia);
+            groupBox2.Controls.Add(tb_Tensanpham);
+            groupBox2.Controls.Add(tb_Masanpham);
+            groupBox2.Controls.Add(lb_Trangthai);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(lb_Tensanpham);
+            groupBox2.Controls.Add(lb_Masanpham);
+            groupBox2.Location = new Point(907, 21);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(916, 521);
-            groupBox2.TabIndex = 1;
+            groupBox2.Size = new Size(329, 393);
+            groupBox2.TabIndex = 21;
             groupBox2.TabStop = false;
-            groupBox2.Text = "Hiển thị";
+            groupBox2.Text = "Chi tiết sản phẩm";
             // 
-            // dataGridView1
+            // cmb_Trangthai
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(9, 102);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.RowTemplate.Height = 29;
-            dataGridView1.Size = new Size(901, 413);
-            dataGridView1.TabIndex = 0;
+            cmb_Trangthai.FormattingEnabled = true;
+            cmb_Trangthai.Location = new Point(19, 259);
+            cmb_Trangthai.Name = "cmb_Trangthai";
+            cmb_Trangthai.Size = new Size(270, 28);
+            cmb_Trangthai.TabIndex = 10;
             // 
-            // label1
+            // tb_Gia
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(9, 23);
-            label1.Name = "label1";
-            label1.Size = new Size(200, 31);
-            label1.TabIndex = 1;
-            label1.Text = "Quản lí sản phẩm";
+            tb_Gia.Location = new Point(19, 189);
+            tb_Gia.Name = "tb_Gia";
+            tb_Gia.Size = new Size(270, 27);
+            tb_Gia.TabIndex = 7;
             // 
-            // groupBox3
+            // tb_Tensanpham
             // 
-            groupBox3.Location = new Point(310, 539);
-            groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(916, 227);
-            groupBox3.TabIndex = 2;
-            groupBox3.TabStop = false;
-            groupBox3.Text = "Hình ảnh";
+            tb_Tensanpham.Location = new Point(19, 124);
+            tb_Tensanpham.Name = "tb_Tensanpham";
+            tb_Tensanpham.Size = new Size(270, 27);
+            tb_Tensanpham.TabIndex = 8;
+            // 
+            // tb_Masanpham
+            // 
+            tb_Masanpham.Location = new Point(19, 60);
+            tb_Masanpham.Name = "tb_Masanpham";
+            tb_Masanpham.Size = new Size(270, 27);
+            tb_Masanpham.TabIndex = 9;
+            // 
+            // lb_Trangthai
+            // 
+            lb_Trangthai.AutoSize = true;
+            lb_Trangthai.Location = new Point(19, 236);
+            lb_Trangthai.Name = "lb_Trangthai";
+            lb_Trangthai.Size = new Size(75, 20);
+            lb_Trangthai.TabIndex = 3;
+            lb_Trangthai.Text = "Trạng thái";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(19, 166);
+            label2.Name = "label2";
+            label2.Size = new Size(31, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Giá";
+            // 
+            // lb_Tensanpham
+            // 
+            lb_Tensanpham.AutoSize = true;
+            lb_Tensanpham.Location = new Point(19, 101);
+            lb_Tensanpham.Name = "lb_Tensanpham";
+            lb_Tensanpham.Size = new Size(100, 20);
+            lb_Tensanpham.TabIndex = 5;
+            lb_Tensanpham.Text = "Tên sản phẩm";
+            // 
+            // lb_Masanpham
+            // 
+            lb_Masanpham.AutoSize = true;
+            lb_Masanpham.Location = new Point(19, 37);
+            lb_Masanpham.Name = "lb_Masanpham";
+            lb_Masanpham.Size = new Size(98, 20);
+            lb_Masanpham.TabIndex = 6;
+            lb_Masanpham.Text = "Mã sản phẩm";
+            // 
+            // btn_Capnhat
+            // 
+            btn_Capnhat.Location = new Point(19, 330);
+            btn_Capnhat.Name = "btn_Capnhat";
+            btn_Capnhat.Size = new Size(100, 43);
+            btn_Capnhat.TabIndex = 11;
+            btn_Capnhat.Text = "    ";
+            btn_Capnhat.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(189, 330);
+            button3.Name = "button3";
+            button3.Size = new Size(100, 43);
+            button3.TabIndex = 11;
+            button3.Text = "button2";
+            button3.UseVisualStyleBackColor = true;
             // 
             // Quản_lí_sản_phẩm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1238, 778);
-            Controls.Add(groupBox3);
+            ClientSize = new Size(1248, 776);
             Controls.Add(groupBox2);
-            Controls.Add(groupBox1);
+            Controls.Add(button1);
+            Controls.Add(btn_Quanlithongke);
+            Controls.Add(btn_Quanlicoso);
+            Controls.Add(btn_QLGG);
+            Controls.Add(btn_QLNH);
+            Controls.Add(btn_QLNV);
+            Controls.Add(btn_QLSP);
+            Controls.Add(ptb_Quanlisanpham);
+            Controls.Add(grb_Quanlisanpham);
             Name = "Quản_lí_sản_phẩm";
             Text = "Quản_lí_sản_phẩm";
-            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ((System.ComponentModel.ISupportInitialize)ptb_Quanlisanpham).EndInit();
+            grb_Quanlisanpham.ResumeLayout(false);
+            grb_Quanlisanpham.PerformLayout();
             groupBox2.ResumeLayout(false);
             groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private GroupBox groupBox1;
-        private Button btn_QLKH;
-        private Button btn_QLBH;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button btn_Quanlithongke;
+        private Button btn_Quanlicoso;
+        private Button btn_QLGG;
+        private Button btn_QLNH;
         private Button btn_QLNV;
         private Button btn_QLSP;
         private PictureBox ptb_Quanlisanpham;
-        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private GroupBox grb_Quanlisanpham;
+        private TextBox tb_Timkiem;
+        private ComboBox cmb_Sapxep;
+        private Button btn_Themsanpham;
         private GroupBox groupBox2;
-        private System.ComponentModel.BackgroundWorker backgroundWorker3;
-        private Button button2;
-        private Button button1;
-        private GroupBox groupBox3;
-        private DataGridView dataGridView1;
-        private Label label1;
+        private Button button3;
+        private Button btn_Capnhat;
+        private ComboBox cmb_Trangthai;
+        private TextBox tb_Gia;
+        private TextBox tb_Tensanpham;
+        private TextBox tb_Masanpham;
+        private Label lb_Trangthai;
+        private Label label2;
+        private Label lb_Tensanpham;
+        private Label lb_Masanpham;
     }
 }
