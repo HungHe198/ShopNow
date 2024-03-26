fb45dc9c1606c20b5a80099237e6faa9a53d5e62
HungHe198 <hunglvph45911@fpt.edu.vn>
2024-03-27 04:23:40 +0700

    update SQL

--- Source Code/DAL/REPOSITORIES/Repo.cs
+++ Source Code/DAL/REPOSITORIES/Repo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShopNow.Source.DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace ShopNow.Source_Code.DAL.REPOSITORIES
 {
     class Repo
     {
+        PRO131_EFContext _db = new PRO131_EFContext();
+        public List<Product> GetAllProducts()
+        {
+            return _db.Products.ToList();
+
+        }
+        public Product GetProductById(Guid id) 
+        {  return _db.Products.Find(x => x.Id == id  ); }
     }
 }
