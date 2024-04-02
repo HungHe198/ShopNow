7a80518402245a6fbcc957940f857d797f28e833
aihung3199@gmail.com <aihung3199@gmail.com>
2024-04-02 17:37:35 +0700

    Add_Repository for tables

---
+++ Source Code/DAL/REPOSITORIES/Bill_Detail_Repository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopNow.Source_Code.DAL.REPOSITORIES
+{
+    internal class Bill_Detail_Repository
+    {
+    }
+}
