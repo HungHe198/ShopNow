3ceec486d5dfe554a3250933fa25dfeb2d4ba36f
aihung3199@gmail.com <aihung3199@gmail.com>
2024-04-03 07:24:23 +0700

    update getAll

--- Source Code/DAL/REPOSITORIES/Repo_GetAll.cs
+++ Source Code/DAL/REPOSITORIES/Repo_GetAll.cs
@@ -11,14 +11,26 @@ namespace ShopNow.Source_Code.DAL.REPOSITORIES
     class Repo_GetAll
     {
         PRO131_EFContext _db = new PRO131_EFContext();
-        public List<Product> GetAllProducts()
+        public List<Product> GetAllProducts(string name = null)
         {
-            return _db.Products.ToList();
-
+            if (string.IsNullOrEmpty(name))
+            {
+                // Trả về tất cả sản phẩm chưa bị xóa
+                return _db.Products.Where(p => p.Deleted == false).ToList();
+                // khi bị xóa trường deleted sẽ được chuyển về true và cập nhật ngày được xóa và người xóa
+            }
+            else
+            {
+                // Trả về sản phẩm có tên gần giống và chưa bị xóa
+                return _db.Products
+                           .Where(p => p.ProductName.Contains(name) && p.Deleted == false)
+                           .ToList();
+            }
         }
+
         public List<Bill> GetAllBill()
         {
-            return _db.Bills.ToList();
+            return _db.Bills.Where(b=>b.Deleted == false).ToList();
         }
         public List<BillDetail> GetAllBillDetail()
         {
@@ -48,35 +60,37 @@ namespace ShopNow.Source_Code.DAL.REPOSITORIES
         }
         public List<Discount> GetAllDiscounts()
         {
-            return _db.Discounts.ToList();
+            return _db.Discounts.Where(d => d.Deleted == false).ToList();
 
         }
         public List<Employee> GetAllEmployees()
         {
-            return (_db.Employees.ToList());
+            return _db.Employees.Where(e => e.Deleted == false).ToList();
         }
         public List<Facility> GetAllFacilities()
         {
-            return _db.Facilities.ToList();
+            return _db.Facilities.Where(f => f.Deleted == false).ToList();
         }
         public List<ImportHistory> GetAllImportHistories()
         {
-            return _db.ImportHistories.ToList();
+            return _db.ImportHistories.Where(ih => ih.Deleted == false).ToList();
 
         }
         public List<ProductDetail> GetAllProductDetails()
         {
-            return _db.ProductDetails.ToList();
+            return _db.ProductDetails.Where(pd => pd.Deleted == false).ToList();
         }
         public List<Role> GetAllRoles()
-        { return _db.Roles.ToList(); }
+        {
+            return _db.Roles.ToList();
+        }
         public List<Supplier> GetAllSuppliers()
         {
-            return _db.Suppliers.ToList();
+            return _db.Suppliers.Where(s => s.Deleted == false).ToList();
         }
         public List<User> GetAllUser()
         {
-            return _db.Users.ToList();
+            return _db.Users.Where(b => b.Deleted == false).ToList();
         }
     }
 }
